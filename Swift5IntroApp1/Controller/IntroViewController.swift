25f56ecaca7b395919b1c86d24daea53f85c7739
kobashou06 <kobashou06@gmail.com>
2021-04-17 00:30:25 +0900

    git add .

--- Swift5IntroApp1/Controller/IntroViewController.swift
+++ Swift5IntroApp1/Controller/IntroViewController.swift
@@ -70,16 +70,5 @@ class IntroViewController: UIViewController, UIScrollViewDelegate{
         }
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
